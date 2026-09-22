c46d9852bf7070e5f9e70ec764c11d22286a0b03
davidpovarsky <davidpovarski1@gmail.com>
2026-09-22 19:28:09 +0300

    Connect isolated home source layer

--- vreader/Views/LibraryView.swift
+++ vreader/Views/LibraryView.swift
@@ -179,6 +179,7 @@ struct LibraryView: View {
                 resolvedGeneralChatVM: resolvedGeneralChatVM
             ))
         }
+        .modifier(HomeSourceLayer())
     }
 
     // MARK: - Constants
